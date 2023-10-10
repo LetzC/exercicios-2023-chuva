006f3a211517a8f4060e8f3423eab4f9f9eeba7f
Leticia <leticiacoutolx@gmail.com>
2023-10-10 14:58:39 -0300

    Funcionalidade readMore e showComments

--- ts/src/app/app.component.ts
+++ ts/src/app/app.component.ts
@@ -8,3 +8,50 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'DevChuva';
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+  const pointsSummary = document.getElementById("points-summary") as HTMLElement;
+  const textMoreSummary = document.getElementById("more-summary") as HTMLElement;
+
+  const pointsMessage = document.getElementById("points-message") as HTMLElement;
+  const textMoreMessage = document.getElementById("more-message") as HTMLElement;
+
+  const btnShowMore = document.querySelector(".btn-show-more") as HTMLButtonElement;
+  const summaryContent = document.querySelector('.summary-content') as HTMLParagraphElement;
+  const answeredTopic = document.querySelector(".answered-topic") as HTMLElement;
+  const commentsContainer = document.querySelector(".comments-container") as HTMLElement;
+
+  function readMore(trigger: HTMLElement, target1: HTMLElement, target2?: HTMLElement) {
+    if (target1.style.display === 'none' || target1.style.display === '') {
+      target1.style.display = 'inline';
+      trigger.style.display = 'none';
+
+      if (target2) {
+        target2.style.display = 'inline';
+      }
+    } else {
+      target1.style.display = 'none';
+      trigger.style.display = 'inline';
+
+      if (target2) {
+        target2.style.display = 'none';
+      }
+    }
+  }
+
+  function showComments(textMoreMessage: HTMLElement, commentsContainer: HTMLElement, pointsMessage: HTMLElement) {
+    if (commentsContainer.style.display === 'none' || commentsContainer.style.display === '') {
+      commentsContainer.style.display = 'flex';
+      textMoreMessage.style.display = 'flex';
+      pointsMessage.style.display = 'none';
+
+    } else {
+      commentsContainer.style.display = 'none';
+      textMoreMessage.style.display = 'none';
+      pointsMessage.style.display = 'inline';
+    }
+  }
+
+  summaryContent?.addEventListener('click', () => readMore(textMoreSummary, btnShowMore, pointsSummary));
+  answeredTopic?.addEventListener('click', () => showComments(textMoreMessage, commentsContainer, pointsMessage));
+})
