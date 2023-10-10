581893642198454213bdd4bc3fc94eae8bcb4573
Leticia <leticiacoutolx@gmail.com>
2023-10-10 15:36:52 -0300

    Refatorando e adicionando showForm

--- ts/src/app/app.component.ts
+++ ts/src/app/app.component.ts
@@ -10,16 +10,29 @@ export class AppComponent {
 }
 
 document.addEventListener('DOMContentLoaded', function () {
+  // Elementos relacionados ao Summary
+  const summaryContent = document.querySelector('.summary-content') as HTMLParagraphElement;
+
   const pointsSummary = document.getElementById("points-summary") as HTMLElement;
   const textMoreSummary = document.getElementById("more-summary") as HTMLElement;
+  const btnShowMore = document.querySelector(".btn-show-more") as HTMLButtonElement;
+
+  // Elementos relacionados á área de comments
+  const answeredTopic = document.querySelector(".answered-topic") as HTMLElement;
+  const commentsContainer = document.querySelector(".comments-container") as HTMLElement;
 
   const pointsMessage = document.getElementById("points-message") as HTMLElement;
   const textMoreMessage = document.getElementById("more-message") as HTMLElement;
 
-  const btnShowMore = document.querySelector(".btn-show-more") as HTMLButtonElement;
-  const summaryContent = document.querySelector('.summary-content') as HTMLParagraphElement;
-  const answeredTopic = document.querySelector(".answered-topic") as HTMLElement;
-  const commentsContainer = document.querySelector(".comments-container") as HTMLElement;
+  // Elementos relacionados á create topic
+  const containerCreateTopic = document.querySelector(".container-create-topic") as HTMLElement;
+  const btnCreateTopic = document.querySelector(".btn-create-topic") as HTMLButtonElement;
+
+
+  // Elementos relacionados ao form
+  const containerForm = document.querySelector(".container-form") as HTMLElement;
+  const btnSend = document.querySelector(".container-button-send")
+
 
   function readMore(trigger: HTMLElement, target1: HTMLElement, target2?: HTMLElement) {
     if (target1.style.display === 'none' || target1.style.display === '') {
@@ -42,7 +55,7 @@ document.addEventListener('DOMContentLoaded', function () {
   function showComments(textMoreMessage: HTMLElement, commentsContainer: HTMLElement, pointsMessage: HTMLElement) {
     if (commentsContainer.style.display === 'none' || commentsContainer.style.display === '') {
       commentsContainer.style.display = 'flex';
-      textMoreMessage.style.display = 'flex';
+      textMoreMessage.style.display = 'inline';
       pointsMessage.style.display = 'none';
 
     } else {
@@ -52,6 +65,18 @@ document.addEventListener('DOMContentLoaded', function () {
     }
   }
 
+  function showForm(containerCreateTopic: HTMLElement, containerForm: HTMLElement) {
+    if (containerForm.style.display === 'none' || containerForm.style.display === '') {
+      containerForm.style.display = 'flex';
+      containerCreateTopic.style.display = 'none';
+    } else {
+      containerCreateTopic.style.display = 'none';
+      containerForm.style.display = 'flex';
+    }
+  }
+
+
   summaryContent?.addEventListener('click', () => readMore(textMoreSummary, btnShowMore, pointsSummary));
   answeredTopic?.addEventListener('click', () => showComments(textMoreMessage, commentsContainer, pointsMessage));
+  btnCreateTopic?.addEventListener('click', () => showForm(containerCreateTopic, containerForm))
 })
